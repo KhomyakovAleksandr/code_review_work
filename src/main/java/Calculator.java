50a5d28ce6b549054de3182690ecf30ce364d805
YouWereCreatedToPushTheButton1231 <khomyakov.a.an.2004@mail.ru>
2026-02-16 00:43:32 +0300

    Khomyakov2: implemented solver and update README

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,24 @@
 public class Calculator {
-    public int add(int a, int b){
-        //TODO inser your realisation in method add
+    public int add(int a, int b) {
+        int result = a + b;
+        return result;
     }
-    public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+    public int dif(int a, int b) {
+        return a - b;
     }
-    public int div(int a, int b){
-        //TODO inser your realisation in method div
+    public int div(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed");
+        }
+        return a / b;
     }
-    public int times(int a, int b){
-        //TODO inser your realisation in method times
+    public int times(int a, int b) {
+        return a * b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+    public int solver() {
+        // Уравнение: (50 - 10) / 4 + 5
+        int difference = dif(50, 10);
+        int division = div(difference, 4);
+        return add(division, 5);
     }
 }
